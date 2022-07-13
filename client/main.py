cd24234dd35124d8643c307672f624efc5cda98d
yasaman <45336465+yasmansh@users.noreply.github.com>
2022-07-13 18:45:42 +0430

    Add files via upload

--- client/main.py
+++ client/main.py
@@ -1,15 +1,41 @@
 import socket
+import datetime
+
+
+def get_timestamp():
+    ct = datetime.datetime.now()  # current time
+    ts = ct.timestamp()  # timestamp of current time
+    return ts
+
 
 host = ""
 port = 0
 with open("configuration_ip_port", "r") as f:
     host = f.readline().strip()
     port = int(f.readline().strip())
-print("ip=" + str(host) + "\tport=" + str(port))
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
+f = open("..\server\server_pub_key.txt", "r")
+public_key_server = f.read()
+f.close()
+
+while True:
+    try:
+        response = client.recv(2048).decode()
+        print(response)
+        if response[-1] == '|':
+            continue
+        command = input()
+        command = command + '$' + str(get_timestamp())
+        client.send(str.encode(command))
+
+
+    except Exception as e:
+        print("Error")
+        break
+"""
 response = client.recv(2048)
 username = input(response.decode())
 client.send(str.encode(username))
@@ -18,13 +44,14 @@ response = client.recv(2048)
 password = input(response.decode())
 client.send(str.encode(password))
 
-response = client.recv(2048)
-response = response.decode()
+response = client.recv(2048).decode()
 print(response)
+"""
+
 ''' Response : Status of Connection :
-	1 : Registeration successful 
-	2 : Login Successful
-	3 : Login Failed
+     > Registration successful 
+	 > Login successful
+	 > Incorrect username or password.
 '''
 
 client.close()
--- server/server.py
+++ server/server.py
@@ -1,48 +1,139 @@
 import socket
 import threading
 import hashlib
+import rsa
+import datetime
+
+
+def get_timestamp():
+    ct = datetime.datetime.now()  # current time
+    ts = ct.timestamp()  # timestamp of current time
+    return ts
+
+
+"""
+ct = datetime.datetime.now()    # current time
+ts = ct.timestamp() # timestamp of current time
+print("timestamp:", ts)
+"""
+
+public_key, private_key = rsa.newkeys(512)
+f = open("server_pub_key.txt", "a")  # append mode
+f.write(str(public_key))
+f.close()
 
 host = ""
 port = 0
 with open("..\client\configuration_ip_port", "r") as f:
     host = f.readline().strip()
     port = int(f.readline().strip())
-print("ip=" + str(host) + "\tport=" + str(port))
+# print("ip=" + str(host) + "\tport=" + str(port))
 
 server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-num_of_connection = 0
+
 try:
     server_socket.bind((host, port))
 except socket.error as e:
     print(str(e))
 
-print('Waitiing for a connection...')
-server_socket.listen(5)
+print('Waiting for a connection...')
+server_socket.listen(20)
 user_pass = {}
+user_info = []  # (username, first_name, last_name)
 
 
-def threaded_client(connection):  # Authentication
-    connection.send(str.encode('Please enter your username: '))
-    username = connection.recv(2048)
-    username = username.decode()
-    connection.send(str.encode('Please enter your password: '))
-    password = connection.recv(2048)
-    password = password.decode()
-
-    password = hashlib.sha256(str.encode(password)).hexdigest()  # Password hash using SHA256
-    if username not in user_pass:  # Registeration
-        user_pass[username] = password
-        connection.send(str.encode('Registeration Successful'))
-    else:  # Login
-        if (user_pass[username] == password):
-            connection.send(str.encode('Login Successful'))
-            print('Connected : ', username)
-        else:
-            connection.send(str.encode('Login Failed'))
-            print('Connection denied : ', username)
+def threaded_client(client):  # Authentication
     while True:
-        break
-    connection.close()
+        client.send(str.encode('1. Sign Up\n2. Login\n3. Exit'))
+        command = client.recv(2048).decode()
+        if check_freshness(command):
+            command = command[:str(command).index('$')]
+        else:
+            client.send(str.encode('Please try again later.\n|'))
+            continue
+
+        if command == '1':
+            client.send(str.encode('Please enter your first name: '))
+            command = client.recv(2048).decode()
+            if check_freshness(command):
+                first_name = command[:str(command).index('$')]
+            else:
+                client.send(str.encode('Please try again later.\n|'))
+                continue
+            client.send(str.encode('Please enter your last name: '))
+            command = client.recv(2048).decode()
+            if check_freshness(command):
+                last_name = command[:str(command).index('$')]
+            else:
+                client.send(str.encode('Please try again later.\n|'))
+                continue
+            client.send(str.encode('Please enter your username: '))
+            command = client.recv(2048).decode()
+            if check_freshness(command):
+                username = command[:str(command).index('$')]
+            else:
+                client.send(str.encode('Please try again later.\n|'))
+                continue
+            while username == 'root' or username in user_pass:
+                client.send(str.encode('This username is already existed. Please enter another one: '))
+                command = client.recv(2048).decode()
+                if check_freshness(command):
+                    username = command[:str(command).index('$')]
+                else:
+                    client.send(str.encode('Please try again later.\n|'))
+                    continue  ###
+
+            client.send(str.encode('Please enter your password: '))
+            command = client.recv(2048).decode()
+            if check_freshness(command):
+                password = command[:str(command).index('$')]
+            else:
+                client.send(str.encode('Please try again later.\n|'))
+                continue
+
+            # Password hash using SHA256
+            password = hashlib.sha256(str.encode(password)).hexdigest()
+            user_pass[username] = password
+            user_info.append((username, first_name, last_name))
+            client.send(str.encode('Registration successful|'))
+            continue  # Back to menu
+
+        elif command == '2':
+            client.send(str.encode('Please enter your username: '))
+            command = client.recv(2048).decode()
+            if check_freshness(command):
+                username = command[:str(command).index('$')]
+            else:
+                client.send(str.encode('Please try again later.\n|'))
+                continue
+            client.send(str.encode('Please enter your password: '))
+            command = client.recv(2048).decode()
+            if check_freshness(command):
+                password = command[:str(command).index('$')]
+            else:
+                client.send(str.encode('Please try again later.\n|'))
+                continue
+            password = hashlib.sha256(str.encode(password)).hexdigest()
+            if (username in user_pass and user_pass[username] == password):
+                client.send(str.encode('Login successful|'))
+                print('Connected : ', username)
+            else:
+                client.send(str.encode('Incorrect username or password.|\n'))
+                continue  # Back to menu
+
+        elif command == '3':
+            print('client disconnected.|')
+            break
+        else:
+            print("Not understand|")
+    client.close()
+
+
+def check_freshness(command):
+    ts = str(command)[str(command).index('$') + 1:]
+    if (get_timestamp() - float(ts)) <= 10:  # Protection against repeated attacks
+        return True
+    return False
 
 
 while True:
@@ -52,6 +143,4 @@ while True:
         args=(Client,)
     )
     client_handler.start()
-    num_of_connection += 1
-    print('Connection Request: ' + str(num_of_connection))
 ServerSocket.close()
